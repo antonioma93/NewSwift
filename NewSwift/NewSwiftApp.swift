6fad5c7dab66ae922c584486559f6d8ad987299a
Antonio Massa <antonio.massa@zehus.it>
2021-06-08 16:45:12 +0200

    Initial Commit

---
+++ NewSwift/NewSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewSwiftApp.swift
+//  NewSwift
+//
+//  Created by Massa Antonio on 08/06/21.
+//
+
+import SwiftUI
+
+@main
+struct NewSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
